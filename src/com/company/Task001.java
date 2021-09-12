4189bf67e6b5691179e0250fad213dffd9ca2b42
Gerabydda <egor.bershitsky@yandex.ru>
2021-09-12 15:23:39 +0300

    First commit

---
+++ src/com/company/Task001.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.*;
+public class Task001 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter min:");
+        System.out.println(in.nextInt() * 60 + " Sec");
+    }
+}
